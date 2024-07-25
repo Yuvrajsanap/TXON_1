31be245e91374c7d7ba598a37baecf428c0f1dc9
Yuvrajsanap <125144651+Yuvrajsanap@users.noreply.github.com>
2024-07-25 16:07:35 +0530

    Delete assignment-6 sql file directory

--- assignment-6 sql file/question1.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS Assignment6db;
-
-USE Assignment6db;
-
-select * from assjdbc1;
--- assignment-6 sql file/question45.sql
+++
@@ -1,9 +0,0 @@
-
-use Assignment6db;
-
-CREATE TABLE ass45jdbc (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    age INT
-);
-select * from ass45jdbc;
