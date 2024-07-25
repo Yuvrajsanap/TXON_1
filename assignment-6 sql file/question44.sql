c576e531ef27e7faf4f1ac923a92e3fa1d564583
Yuvrajsanap <125144651+Yuvrajsanap@users.noreply.github.com>
2024-07-25 16:02:37 +0530

    Add files via upload

---
+++ assignment-6 sql file/question44.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS Assignment6db;
+
+use Assignment6db;
+
+CREATE TABLE ass44jdbc (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    image BLOB
+);
+
+select * from ass44jdbc;
